2b9215c5ae98bbe24ab92c46bef4aa3fb6338529
Franco <jfrancovergara2502@gmail.com>
2026-06-22 12:51:34 -0500

    corregi en security la parte del swagger

--- proyecto-dswii/src/main/java/com/proyecto/security/SecurityConfig.java
+++ proyecto-dswii/src/main/java/com/proyecto/security/SecurityConfig.java
@@ -29,9 +29,13 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
 
             		
-            		// SWAGGER - publico
-
-            	.requestMatchers("/swagger-ui/ ** ", "/v3/api-docs/ ** ").permitAll()
+            	// SWAGGER - publico
+            	.requestMatchers(
+            	    "/swagger-ui/**",
+            	    "/swagger-ui.html",
+            	    "/v3/api-docs/**",
+            	    "/v3/api-docs.yaml"
+            	).permitAll()	
             		
             		
                 // ══════════════════════════════════════════
